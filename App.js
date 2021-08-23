2bf189bdd73604ac7ceff58ae50f18704143a3cf
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-23 13:40:37 +0100

    Navscreen implemented and styled with tailwind

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import { Provider } from "react-redux"
+import HomeScreen from "./screens/HomeScreen"
 import { store } from "./store"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber!!!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   )
 }
