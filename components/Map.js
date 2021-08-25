2cb0579489604c4e3d29a5d3806111c71584992d
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-25 04:37:16 +0100

    display route line from origin to destination

--- components/Map.js
+++ components/Map.js
@@ -3,10 +3,13 @@ import { StyleSheet, Text, View } from "react-native"
 import MapView, { Marker } from "react-native-maps"
 import { useSelector } from "react-redux"
 import tw from "tailwind-react-native-classnames"
-import { selectOrigin } from "../slices/navSlice"
+import { selectDestination, selectOrigin } from "../slices/navSlice"
+import { GOOGLE_MAPS_APIKEY } from "@env"
+import MapViewDirections from "react-native-maps-directions"
 
 const Map = () => {
   const origin = useSelector(selectOrigin)
+  const destination = useSelector(selectDestination)
 
   return (
     <MapView
@@ -19,6 +22,17 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          lineDashPattern={[0]}
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
