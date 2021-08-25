2307bf4cab7b61339e918b1237142701078b9d65
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-25 04:53:31 +0100

    useRef to resize map and show both markers

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { StyleSheet, Text, View } from "react-native"
 import MapView, { Marker } from "react-native-maps"
 import { useSelector } from "react-redux"
@@ -10,9 +10,20 @@ import MapViewDirections from "react-native-maps-directions"
 const Map = () => {
   const origin = useSelector(selectOrigin)
   const destination = useSelector(selectDestination)
+  const mapRef = useRef(null)
+
+  useEffect(() => {
+    if (!origin || !destination) return
+
+    //Zoom to fit markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    })
+  }, [origin, destination])
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -42,6 +53,18 @@ const Map = () => {
           title="Ride From"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Ride From"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
